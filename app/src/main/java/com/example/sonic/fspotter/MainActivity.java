cfc1be50695a64fadaf5845419a5ee06c865cd58
sonic <sebastian.saier@web.de>
2015-06-15 15:31:19 +0200

    Test master.

--- app/src/main/java/com/example/sonic/fspotter/MainActivity.java
+++ app/src/main/java/com/example/sonic/fspotter/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends FragmentActivity {
 
-
+    // jbkbfkfkef
     //kjncknjfe
     
     public static final String TAG = MainActivity.class.getSimpleName();
